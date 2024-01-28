3a5356ff884a2a7cc160e6d52d7c64a5dd475466
moecasts <moecasts.caster@gmail.com>
2024-01-29 01:43:31 +0800

    feat(typography): add typography component

---
+++ packages/casts-typography/src/components/index.ts
@@ -0,0 +1,7 @@
+import './styles/typography.scss';
+
+export * from './heading';
+export * from './paragraph';
+export * from './text';
+export * from './types';
+export * from './typography';
---
+++ packages/casts-typography/src/components/paragraph.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+import { useConfig } from '@casts/config-provider';
+import clsx from 'clsx';
+
+import { ParagraphProps } from './types';
+
+import './styles/typography.scss';
+
+export const Paragraph: FC<ParagraphProps> = (props) => {
+  const { children } = props;
+  const { getPrefixCls } = useConfig();
+  const prefixCls = getPrefixCls('typography');
+
+  const classes = clsx(prefixCls);
+
+  return <div className={classes}>{children}</div>;
+};
