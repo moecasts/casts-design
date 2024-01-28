c0aa50f040f3e339571ebc35f652cb71603e95ed
moecasts <moecasts.caster@gmail.com>
2024-01-29 01:42:49 +0800

    feat(button): add button component

---
+++ packages/casts-button/src/components/default-props.ts
@@ -0,0 +1,9 @@
+import { ButtonProps } from './types';
+
+export const defaultButtonProps: Partial<ButtonProps> = {
+  variant: 'contained',
+  size: 'medium',
+  shape: 'round',
+  theme: 'brand',
+  type: 'button',
+};
