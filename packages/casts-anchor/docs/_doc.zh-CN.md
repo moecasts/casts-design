c5ae1baca5f1c0eef3434046678fa64014899102
moecasts <moecasts.caster@gmail.com>
2024-01-29 01:42:44 +0800

    feat(anchor): add anchor component

---
+++ packages/casts-anchor/docs/_doc.zh-CN.md
@@ -0,0 +1,12 @@
+---
+title: @casts/anchor
+group:
+  title: 通用
+nav:
+  title: 组件
+  path: /components
+---
+
+# @casts/anchor
+
+TODO
---
+++ packages/casts-anchor/src/components/anchor-context.tsx
@@ -0,0 +1,164 @@
+import {
+  createContext,
+  FC,
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import { filter, getScroll, isEmpty, scrollTo } from '@casts/common';
+
+import { AnchorContextValue, AnchorProviderProps } from './types';
+
+export const AnchorContext = createContext<AnchorContextValue>({} as any);
+
+export const useAnchorContext = () => useContext(AnchorContext);
+
+export const AnchorProvider: FC<AnchorProviderProps> = (props) => {
+  const { children } = props;
+
+  const value = useAnchorProvider();
+
+  return (
+    <AnchorContext.Provider value={value}>{children}</AnchorContext.Provider>
+  );
+};
+
+const linkRegex = /#([\S ]+)$/;
+
+export const useAnchorProvider = () => {
+  const container = window;
+  const [links, setLinks] = useState<string[]>([]);
+
+  const [activeLink, setActiveLink] = useState<string>();
+
+  const animatingRef = useRef(false);
+  /* --------------------------------- handle links ---------------------------------------- */
+  const addLink = (link: string) => {
+    if (links.includes(link)) {
+      return;
+    }
+
+    setLinks((prev) => [...prev, link]);
+  };
+
+  const removeLink = (link: string) => {
+    if (!links.includes(link)) {
+      return;
+    }
+
+    setLinks((prev) => filter(prev, link));
+  };
+
+  /* --------------------------------- handle scroll events ---------------------------------------- */
+  const handleScroll = useCallback(() => {
+    if (isEmpty(links) || animatingRef.current) {
+      return;
+    }
+
+    const activeLink = getCurrentActiveLink(links);
+    setActiveLink(activeLink);
+  }, [links]);
+
+  const setCurrentActiveLink = (link: string) => {
+    handleScrollTo(link);
+  };
+
+  const handleScrollTo = (link: string) => {
+    setActiveLink(link);
+    const linkMatch = linkRegex.exec(link);
+    if (!linkMatch) {
+      return;
+    }
+
+    const target = document.querySelector(link) as HTMLElement | undefined;
+    if (!target) {
+      return;
+    }
+
+    const scrollTop = getScroll(container, true);
+    const elementOffsetTop = getOffsetTop(target, container);
+    const y = scrollTop + elementOffsetTop;
+
+    animatingRef.current = true;
+    scrollTo(y, {
+      getContainer: () => window,
+      callback: () => {
+        animatingRef.current = false;
+      },
+      duration: 200,
+    });
+  };
+
+  const getCurrentActiveLink = (links: string[], offsetTop = 0, bounds = 5) => {
+    const linkSections: Array<{ link: string; top: number }> = [];
+
+    links.forEach((link) => {
+      const linkMatch = linkRegex.exec(link);
+
+      if (!linkMatch) {
+        return;
+      }
+
+      const target = document.querySelector(link) as HTMLElement | undefined;
+
+      if (!target) {
+        return;
+      }
+
+      const top = getOffsetTop(target, container);
+
+      if (top >= offsetTop + bounds) {
+        return;
+      }
+
+      linkSections.push({ link, top });
+    });
+
+    if (isEmpty(linkSections)) {
+      return;
+    }
+
+    const maxSection = linkSections.reduce((prev, curr) =>
+      curr.top > prev.top ? curr : prev,
+    );
+    return maxSection.link;
+  };
+
+  const getOffsetTop = (
+    element: HTMLElement,
+    container: HTMLElement | Window,
+  ): number => {
+    if (!element.getClientRects().length) {
+      return 0;
+    }
+
+    const rect = element.getBoundingClientRect();
+
+    if (rect.width || rect.height) {
+      if (container === window) {
+        container = element.ownerDocument!.documentElement!;
+        return rect.top - container.clientTop;
+      }
+      return rect.top - (container as HTMLElement).getBoundingClientRect().top;
+    }
+
+    return rect.top;
+  };
+
+  useEffect(() => {
+    container.addEventListener('scroll', handleScroll);
+
+    return () => {
+      container.removeEventListener('scroll', handleScroll);
+    };
+  }, [handleScroll]);
+
+  return {
+    activeLink,
+    addLink,
+    removeLink,
+    setCurrentActiveLink,
+  };
+};
