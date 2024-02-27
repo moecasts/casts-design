b51c5d66f56983efd1f19b76a2830a725ee9915c
moecats <moecasts.caster@gmail.com>
2024-02-27 10:31:18 +0800

    feat(*): upgrade vitest

--- packages/casts-standard/src/vite/vite.config.ts
+++ packages/casts-standard/src/vite/vite.config.ts
@@ -1,16 +1,17 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, mergeConfig } from 'vitest/config';
 
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: path.join(__dirname, 'vitest.setup'),
-    css: true,
+export default mergeConfig(
+  {
+    plugins: [react()],
   },
-});
+  defineConfig({
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: path.join(__dirname, 'vitest.setup'),
+      css: true,
+    },
+  }),
+);
