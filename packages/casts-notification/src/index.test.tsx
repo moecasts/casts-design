b03e02df914e5e5657efc790d3a14fe459c0b85a
moecats <moecasts.caster@gmail.com>
2024-04-11 12:19:02 +0800

    feat(notification): add notification component

---
+++ packages/casts-notification/src/index.test.tsx
@@ -0,0 +1,8 @@
+import {} from '@testing-library/react';
+import { describe, it } from 'vitest';
+
+describe('@casts/notification', () => {
+  it('test', () => {
+    console.log('test');
+  });
+});
