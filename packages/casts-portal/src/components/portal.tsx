ad17c3d336a76b64421e345944fabec99393b315
moecasts <moecasts.caster@gmail.com>
2024-06-12 02:00:03 +0800

    feat(portal): add portal component

---
+++ packages/casts-portal/src/components/portal.tsx
@@ -0,0 +1,34 @@
+import {
+  forwardRef,
+  ReactNode,
+  useEffect,
+  useImperativeHandle,
+  useMemo,
+} from 'react';
+import { createPortal } from 'react-dom';
+import { isCanUseDocument } from '@casts/common';
+
+export type PortalProps = {
+  children?: ReactNode;
+};
+
+export const Portal = forwardRef((props: PortalProps, ref) => {
+  const { children } = props;
+  const container = useMemo(() => {
+    const node = document.createElement('div');
+    node.classList.add('cds-portal');
+    return node;
+  }, []);
+
+  useEffect(() => {
+    container && document.body.appendChild(container);
+
+    return () => {
+      container?.parentElement?.removeChild(container);
+    };
+  }, [container]);
+  useImperativeHandle(ref, () => container);
+  return isCanUseDocument() ? createPortal(children, container) : null;
+});
+
+Portal.displayName = 'Portal';
