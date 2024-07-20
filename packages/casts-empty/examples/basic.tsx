fcbee3c1ee67405181482bd6d8fb3713e5849e76
moecasts <moecasts.caster@gmail.com>
2024-07-20 18:37:12 +0800

    feat(empty): add empty component

---
+++ packages/casts-empty/examples/basic.tsx
@@ -0,0 +1,9 @@
+// @ts-ignore example should import React
+import React from 'react';
+import { Empty } from '@casts/empty';
+
+const EmptyBasicDemo = () => {
+  return <Empty />;
+};
+
+export default EmptyBasicDemo;
