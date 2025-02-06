659b7ac62d416bae83371e8e68c360b30d4b91ce
moecasts <moecasts.caster@gmail.com>
2025-02-07 01:38:11 +0800

    feat(button): add button component

---
+++ packages/casts-button/examples/different-shape-buttons.tsx
@@ -0,0 +1,16 @@
+// @ts-ignore example should import React
+import React from 'react';
+import { Button } from '@casts/button';
+import { Space } from '@casts/space';
+
+import '@casts/button/dist/esm/styles.scss';
+
+const DifferentShapeButtonsDemo = () => (
+  <Space wrap>
+    <Button shape="round">Button</Button>
+    <Button shape="square">Button</Button>
+    <Button shape="pill">Button</Button>
+  </Space>
+);
+
+export default DifferentShapeButtonsDemo;
