f0acb67fe4ffb80c42edd0f0d510ef7a348613ff
moecasts <moecasts.caster@gmail.com>
2025-02-07 14:29:47 +0800

    feat(theme-generator): add theme generator component

---
+++ packages/casts-theme-generator/src/components/hooks/use-theme-generator.ts
@@ -0,0 +1,163 @@
+import { CSSProperties, useEffect, useRef } from 'react';
+import {
+  noop,
+  useCircleTransition,
+  useControlled,
+  useDebounceFn,
+} from '@casts/common';
+import { useConfig } from '@casts/config-provider';
+import { clsx } from 'clsx';
+
+import { MainColor, ThemeMode, UseThemeGeneratorProps } from '../types';
+import {
+  appendThemeVariablesToHead,
+  generatePalettes,
+  toCssVariables,
+} from '../utils/generate-palette';
+
+export const useThemeGenerator = (props: UseThemeGeneratorProps) => {
+  const { className, style, addThemeCodeOnMounted } = props;
+
+  const [visible, onVisibleChange] = useControlled(
+    props,
+    'visible',
+    props.onVisibleChange || noop,
+  );
+
+  const { getPrefixCls } = useConfig();
+
+  /* --------------------------------- classes and styles ---------------------------------------- */
+  const prefixCls = getPrefixCls('theme-generator');
+
+  const classes = clsx(prefixCls, className);
+  const styles: CSSProperties = { ...style };
+
+  const drawerClasses = `${prefixCls}-drawer`;
+
+  const itemClasses = `${prefixCls}-item`;
+
+  const itemTitleClasses = `${itemClasses}-title`;
+
+  const itemContentClasses = `${itemClasses}-content`;
+
+  const pickerClasses = `${prefixCls}-picker`;
+
+  const pickerPreviewClasses = `${pickerClasses}-preview`;
+  const pickerTextClasses = `${pickerClasses}-text`;
+
+  /* --------------------------------- states ---------------------------------------- */
+  const [mode, setMode] = useControlled(
+    props,
+    'mode',
+    props.onModeChange || noop,
+    'default',
+  );
+
+  const { circleTransition } = useCircleTransition();
+  const generatorRef = useRef<HTMLElement>(null);
+
+  const [mainColors, setMainColors] = useControlled(
+    props,
+    'mainColors',
+    props.onMainColorsChange || noop,
+    [],
+  );
+
+  /* --------------------------------- events ---------------------------------------- */
+
+  const debounceGeneratePalettes = useDebounceFn(
+    (mainColors: MainColor[], mode: ThemeMode = 'default') => {
+      const update = () => {
+        const reverse = mode === 'dark' ? true : false;
+
+        const palettes = generatePalettes(mainColors, reverse);
+
+        const cssCodes = toCssVariables(palettes);
+        setTimeout(() => {
+          document.documentElement.setAttribute('theme-mode', mode);
+          document.documentElement.setAttribute('theme-palette', 'custom');
+          appendThemeVariablesToHead(cssCodes);
+        }, 0);
+      };
+
+      circleTransition({
+        ref: generatorRef,
+        update,
+      });
+    },
+    {
+      wait: 300,
+    },
+  );
+
+  const handleMainColorsChange = (payload: { name: string; color: string }) => {
+    const { name, color } = payload;
+
+    const newColors = [...mainColors];
+
+    const target = newColors.find((item) => item.name === name);
+
+    if (target) {
+      target.color = color;
+    }
+
+    setMainColors(newColors);
+
+    debounceGeneratePalettes.run(newColors, mode);
+  };
+
+  const handleModeChange = (reverse: boolean) => {
+    const mode = reverse ? 'dark' : 'default';
+    setMode(mode);
+
+    debounceGeneratePalettes.run(mainColors, mode);
+  };
+
+  // generate current theme when component mounted
+  useEffect(() => {
+    if (!addThemeCodeOnMounted) {
+      return;
+    }
+
+    debounceGeneratePalettes.run(mainColors, mode);
+    debounceGeneratePalettes.flush();
+
+    // component mounted should disabled transition,
+    // debounceGeneratePalettes is a setTimeout function,
+    // so, we can execute the following code after debounceGeneratePalettes
+    const noTransitionStyleCode = '* { transition: none !important; }';
+
+    const style = document.createElement('style');
+    style.innerHTML = noTransitionStyleCode;
+
+    document.head.appendChild(style);
+    setTimeout(() => {
+      style.remove();
+    }, 10);
+  }, []);
+
+  return {
+    classes,
+    styles,
+
+    drawerClasses,
+
+    itemClasses,
+    itemTitleClasses,
+    itemContentClasses,
+
+    pickerClasses,
+    pickerPreviewClasses,
+    pickerTextClasses,
+
+    mainColors,
+    handleMainColorsChange,
+    reverse: mode === 'dark',
+    handleModeChange,
+
+    visible,
+    onVisibleChange,
+
+    generatorRef,
+  };
+};
