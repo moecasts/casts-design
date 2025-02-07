ccc8ae07b136a5e2512fbdcb9d2db72308fd13f9
moecasts <moecasts.caster@gmail.com>
2025-02-07 14:29:00 +0800

    feat(radio): add radio component

---
+++ packages/casts-radio/examples/group.tsx
@@ -0,0 +1,27 @@
+// @ts-ignore example should import React
+import React from 'react';
+import { ChangeEventHandler, Radio, RadioGroup } from '@casts/radio';
+import { Space } from '@casts/space';
+
+const GroupDemo = () => {
+  const defaultValue = 2;
+  const onChange: ChangeEventHandler = (value) => {
+    console.log('debug', value);
+  };
+  return (
+    <Space direction="vertical">
+      <RadioGroup
+        defaultValue={defaultValue}
+        onChange={onChange}
+        name="basic-group"
+      >
+        <Radio value={1}>radio1</Radio>
+        <Radio value={2}>radio2</Radio>
+        <Radio value={3}>radio3</Radio>
+        <Radio value={4}>radio4</Radio>
+      </RadioGroup>
+    </Space>
+  );
+};
+
+export default GroupDemo;
---
+++ packages/casts-radio/src/components/types/index.ts
@@ -0,0 +1,34 @@
+import { ChangeEvent, CSSProperties, MouseEvent, ReactNode } from 'react';
+
+export type RadioValue = boolean | string | number;
+
+export type ChangeEventContext = {
+  type?: 'input';
+  e: ChangeEvent<HTMLInputElement> | MouseEvent<HTMLInputElement>;
+};
+
+export type ChangeEventHandler = (
+  value: RadioValue,
+  context: ChangeEventContext,
+) => void;
+
+export type RadioGroupProps = {
+  value?: RadioValue;
+  defaultValue?: RadioValue;
+  children?: ReactNode;
+  className?: string;
+  disabled?: boolean;
+  name?: string;
+  onChange?: ChangeEventHandler;
+  style?: CSSProperties;
+  type?: 'radio' | 'button';
+};
+
+export interface RadioGroupContextProps {
+  disabled?: boolean;
+  group?: boolean;
+  name?: RadioGroupProps['name'];
+  onChange?: ChangeEventHandler;
+  type: 'radio' | 'button';
+  value?: RadioValue;
+}
