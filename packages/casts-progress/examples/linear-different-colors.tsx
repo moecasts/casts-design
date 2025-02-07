010799226cb6d3dd02cbdb96293cdce4ca912954
moecasts <moecasts.caster@gmail.com>
2025-02-07 14:28:21 +0800

    feat(progress): add progress components

---
+++ packages/casts-progress/examples/linear-different-colors.tsx
@@ -0,0 +1,14 @@
+// @ts-ignore example should import React
+import React from 'react';
+import { LinearProgress } from '@casts/progress';
+import { Space } from '@casts/space';
+
+const LinearDifferentColorDemo = () => {
+  return (
+    <Space direction="vertical" style={{ display: 'flex' }}>
+      <LinearProgress percent={30} color="red" />
+      <LinearProgress percent={50} color="red" trackColor="pink" />
+    </Space>
+  );
+};
+export default LinearDifferentColorDemo;
